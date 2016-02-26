67fffc72de920990a50d68bb40c598facf9c1540
Berich <sarra.berrich@gmail.com>
2016-02-26 10:57:52 +0100

    Implementation of IInstrument interface, Trumpet,Flute classes  according to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Sarra
+ */
+public interface IInstrument {
+    
+    String play();
+
+    public int getSoundVolume();
+
+
+    public String getColor();
+    
+}
